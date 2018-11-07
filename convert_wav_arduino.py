2f8b1daa86b333ba8037543f59469dee28a37a22
keziah <k.milligan.1@research.gla.ac.uk>
2018-11-07 22:09:00 +0000

    Commenting convert_wav_arduino.py

--- convert_wav_arduino.py
+++ convert_wav_arduino.py
@@ -10,50 +10,77 @@ import argparse
 
 
 def make_wavs(filelist, outdir):
+    """ Convert files to wavs that an Arduino can play
     
+        Paramters
+        ---------
+        filelist : str
+            Path to a file containing the file names to be converted
+        outdir : str
+            Path to directory where wavs should be saved
+    """
+    
+    # if wav dir does not exist, make it
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     
+    # wav number (for file name)
     i = 0
     
     with open(filelist) as fileobj:
-        line = ' '
         
-        while True:
+        line = ' ' # non-empty string so it can be used as 'while' condition
+        
+        while line:
             
+            # get next track name
             line = fileobj.readline()
-            line = line.strip()
+            # remove newline character from end
+            line = line.strip() 
             
-            if not line:
-                break
-            
+            # make 'track' variable and escape special characters
             track = line
-            
             track = track.replace(' ', '\ ')
             track = track.replace('(', '\(')
             track = track.replace(')', ')')
             
+            # wav file to write
             outfile = "{}.wav".format(i)
             out = os.path.join(outdir, outfile)
             
+            # run sox
             subprocess.run(["sox", track, "-b", "8", "-r", "32000", "-c", "1", 
                             out])
-            
+    
+            # increment wav number
             i += 1
     
     
 def make_ino_file(inofile, wavdir):
+    """ Make .ino file containing a function to populate an array of file names
     
+        Paramters
+        ---------
+        inofile : str
+            Path to file to
+        wavdir : str
+            Directory where the wavs are
+    """
+    
+    # beginning of file
     out = 'void makeFileArray() {\n'
     
+    # get list of wavs and sort by number
     ls = os.listdir(wavdir)
     ls.sort(key=lambda s:float(s.split('.')[0]))
     
+    # add lines to file
     for i, file in enumerate(ls):
         out += '    fileArray[{}] = "{}";\n'.format(i, file)
         
     out += '}\n'
         
+    # write .ino file
     with open(inofile, 'w') as fileobj:
         fileobj.write(out)
 
